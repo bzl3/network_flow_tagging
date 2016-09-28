8d3aa3c1541dc663ba1a0a8b34b29ab059d3b673
bzl3 <bzl3@cornell.edu>
2016-09-28 08:16:08 -0400

    basic nf_test

--- nf_kmod.c
+++ nf_kmod.c
@@ -1,20 +1,49 @@
-//'Hello World' kernel module, logs call to init_module
-// and cleanup_module to /var/log/messages
+//'Hello World' netfilter hooks example
+//For any packet, we drop it, and log fact to /var/log/messages
 
-#define __KERNEL__
-#define MODULE
+//#undef __KERNEL__
+#include <linux/netfilter_ipv4.h>
+//#define __KERNEL__
 
-#include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/netfilter.h>
+/*
+  #define NF_DROP 0
+  #define NF_ACCEPT 1
+  #define NF_STOLEN 2
+  #define NF_QUEUE 3
+  #define NF_REPEAT 4
+  #define NF_STOP 5
+  #define NF_MAX_VERDICT NF_STOP
+*/
 
-int init_module(void)
+static struct nf_hook_ops nfho;         //struct holding set of hook function options
+
+//function to be called by hook
+unsigned int hook_func(unsigned int hooknum, struct sk_buff **skb, const struct net_device *in,
+                       const struct net_device *out, int (*okfn)(struct sk_buff *))
 {
-   printk(KERN_ERR "Bryan init_module() called\n");
-   return 0;
+  printk(KERN_INFO "packet Received!!!!!!!!\n");      //log to var/log/messages
+  return NF_ACCEPT;                                   //drops the packet
 }
 
-void cleanup_module(void)
+//Called when module loaded using 'insmod'
+int init_module()
 {
-   printk(KERN_ERR "Bryan cleanup_module() called\n");
+  nfho.hook = hook_func;                       //function to call when conditions below met
+//  nfho.hooknum = NF_IP_PRE_ROUTING;            //called right after packet recieved, first hook in Netfilter
+  nfho.hooknum = NF_INET_PRE_ROUTING;
+  nfho.pf = PF_INET;                           //IPV4 packets
+  nfho.priority = NF_IP_PRI_FIRST;             //set to highest priority over all other hook functions
+  nf_register_hook(&nfho);                     //register hook
+
+  return 0;                                    //return 0 for success
+}
+
+//Called when module unloaded using 'rmmod'
+void cleanup_module()
+{
+  nf_unregister_hook(&nfho);                     //cleanup – unregister hook
 }
 
